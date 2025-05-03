808f04299875e96a6fc34d4c83f7cdadaea74ec4
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 22:04:38 +0530

    Update sell_item.java

--- project/src/main/java/com/lk/vau/it/project/ecommerce/model/sell_item.java
+++ project/src/main/java/com/lk/vau/it/project/ecommerce/model/sell_item.java
@@ -3,6 +3,7 @@ package com.lk.vau.it.project.ecommerce.model;
 import jakarta.persistence.Column;
 
 public class sell_item extends item{
+    private String seller_name;
     private int stock;
 
     @Column(length = 1000)
