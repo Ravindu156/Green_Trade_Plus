2bac66fa43a672cb0429b342dfcd95b6c67168c4
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 23:22:23 +0530

    Created Repositories

---
+++ project/src/main/java/com/lk/vau/it/project/ecommerce/repository/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.lk.vau.it.project.ecommerce.repository;
+
+import com.lk.vau.it.project.ecommerce.model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, String> {
+}
