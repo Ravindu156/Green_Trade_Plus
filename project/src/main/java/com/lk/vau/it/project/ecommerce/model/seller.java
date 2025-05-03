4bcf0756ab627b891435c62a04f55dbc89bb0e0c
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 22:09:25 +0530

    update sell_item.java

--- project/src/main/java/com/lk/vau/it/project/ecommerce/model/seller.java
+++ project/src/main/java/com/lk/vau/it/project/ecommerce/model/seller.java
@@ -2,6 +2,7 @@ package com.lk.vau.it.project.ecommerce.model;
 
 public class seller {
     private String seller_name;
+    private String seller_id;
     private String seller_NIC;
     private String seller_address;
     private String seller_email;
