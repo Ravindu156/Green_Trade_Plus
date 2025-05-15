ef5adbacd79d05c304ff80c7d89bdcf1d2fa80ad
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-15 21:20:16 +0530

    Create model for PromoBanner

---
+++ project/src/main/java/com/lk/vau/it/project/ecommerce/model/PromoBanner.java
@@ -0,0 +1,21 @@
+package com.lk.vau.it.project.ecommerce.model;
+
+public class PromoBanner {
+    private Long id;
+    private String imageUrl;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    
+}
